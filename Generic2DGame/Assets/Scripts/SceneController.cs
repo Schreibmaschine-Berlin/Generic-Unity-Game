bfe89731fbb6a543f448356b660c1520e1bd936a
Willow Key <Mercury9791@gmail.com>
2020-09-27 00:34:32 -0700

    Attempt to merge file structure

---
+++ Generic2DGame/Assets/Scripts/SceneController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DisallowMultipleComponent]
+public class SceneController : MonoBehaviour
+{
+    #region Editor Variables
+    #endregion
+
+    #region Private Variables
+    private string p_SceneName;
+    private bool p_IsPaused;
+    #endregion
+
+    #region Public Variables
+    public GameObject pauseScreen;
+    #endregion
+
+    #region Initialization Methods
+    private void Awake()
+    {
+
+    }
+
+    private void Start()
+    {
+        p_SceneName = SceneManager.GetActiveScene().name;
+    }
+    #endregion
+
+    #region Main Updates
+    private void Update()
+    {
+
+    }
+    #endregion
+
+    #region Scene Load and Unload
+    public void AddScene(string name)
+    {
+        SceneManager.LoadSceneAsync(name, LoadSceneMode.Additive);
+    }
+
+    public void RemoveScene(string name)
+    {
+        SceneManager.UnloadSceneAsync(name);
+    }
+    #endregion
+
+    #region Pause
+    // This version of the Pause function does not use additive scene loading.
+    public void Pause()
+    {
+        p_IsPaused = !p_IsPaused;
+        if (p_IsPaused)
+        {
+            Time.timeScale = 0;
+            pauseScreen.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseScreen.SetActive(false);
+        }
+    }
+
+    // This version of the Pause function uses additive scene loading.
+    public void PauseScene(string pauseSceneName)
+    {
+        p_IsPaused = !p_IsPaused;
+        if (p_IsPaused)
+        {
+            Time.timeScale = 0;
+            SceneManager.LoadSceneAsync(pauseSceneName, LoadSceneMode.Additive);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            SceneManager.UnloadSceneAsync(pauseSceneName);
+        }
+    }
+    #endregion
+}
